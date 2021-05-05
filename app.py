8de0d4d719e2e9eb6eb69acd06c0d6ada0f17f16
Isaac Mwendwa <51324520+IsaacMwendwa@users.noreply.github.com>
2021-05-05 19:25:00 +0300

    Fix file download errors

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, current_app
 import os
 from os.path import join, dirname, realpath
 
